325eec518297f9d480f47152f662521c4ed6ca56
yhymaa-gif <yhymaa@gmail.com>
2026-07-26 23:58:54 +0300

    Restore alert cancellation controls

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='bestcare-treatment-plan-v1-20260726-adaptive-sync';
+const CACHE_NAME='bestcare-treatment-plan-v1-20260726-notification-controls';
 const APP_SHELL=[
   './',
   './index.html',
