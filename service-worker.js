fb2c84bc887af3279e2e5ec5499c0e1dd07c3b7c
yhymaa-gif <yhymaa@gmail.com>
2026-07-29 23:47:04 +0300

    Move dental lab entry to patient list

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='bestcare-treatment-plan-v1-20260729-dental-lab';
+const CACHE_NAME='bestcare-treatment-plan-v1-20260729-direct-lab-entry';
 const APP_SHELL=[
   './',
   './index.html',
